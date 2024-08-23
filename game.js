139279fbf6a6a0b6194d23a271f4ef8c21cae27b
soonode97 <soonode951@gmail.com>
2024-08-23 10:28:56 +0900

    Update game.js - 메시지 출력 로직 수정
    메시지를 출력하는 로직을 수정
    1) Battle의 메서드로 initBattleLogs 선언
    2) initBattleLogs 메서드로 로그삽입과 즉시 출력을 동시 진행
    3) 도망가기 일부 수정

--- game.js
+++ game.js
@@ -9,26 +9,27 @@ class Unit {
     this.maxDamage = maxDamage;
     this.unitType = type;
     this.speed = speed;
+    this.isRun = false;   // 도망갔는지 확인하는 불리언
   }
 
   hold(logs) {
-    logs.push(chalk.bgBlack(`[${++logs.length}] ${this.name}이 가만히 있습니다.`));
+    Battle.initBattleLogs(chalk.bgBlack(`[${logs.length}] ${this.name}이 가만히 있습니다.`), logs);
   }
 
   attack(target, logs) {
     const damage = Math.floor(Math.random() * this.minDamage + this.maxDamage);
-    const attackMessage = `[${++logs.length}] ${this.name}이 ${target.name}에게 ${damage}만큼 피해를 입혔습니다.`;
+    const attackMessage = `[${logs.length}] ${this.name}이 ${target.name}에게 ${damage}만큼 피해를 입혔습니다.`;
     target.hp -= damage;
     target.unitType !== 'user'
-      ? console.log(chalk.bgGreen(attackMessage))
-      : console.log(chalk.bgRed(attackMessage));
-      console.log(chalk.bgBlack(`[${++logs.length}] ${target.name}의 HP가 ${target.hp}만큼 남았습니다.`));
+      ? Battle.initBattleLogs(chalk.bgGreen(attackMessage), logs)
+      : Battle.initBattleLogs(chalk.bgRed(attackMessage), logs);
+      Battle.initBattleLogs(chalk.bgBlack(`[${logs.length}] ${target.name}의 HP가 ${target.hp}만큼 남았습니다.`), logs);
   }
 }
 
 class Player extends Unit {
   constructor() {
-    super('Violeto', 50, 10, 10, 'user', 80);
+    super('Violeto', 100, 10, 10, 'user', 80);
     this.escapeChanceRatio = 20;
   }
 
@@ -36,10 +37,11 @@ class Player extends Unit {
   escape(logs) {
     const isEscape = Math.floor(Math.random() * 100);
     if (isEscape > this.escapeChanceRatio) {
-      console.log(chalk.bgGreen(`[${++logs.length}] ${this.name}이 도망치지 못했습니다!!`));
+      Battle.initBattleLogs(chalk.bgGreen(`[${logs.length}] ${this.name}이 도망치지 못했습니다!!`), logs);
+      logs.push();
       return false;
     }
-    console.log(chalk.bgGreen(`[${++logs.length}] ${this.name}이 도망에 성공했습니다!!`));
+    Battle.initBattleLogs(chalk.bgGreen(`[${logs.length}] ${this.name}이 도망에 성공했습니다!!`), logs);
     return true;
   }
 
@@ -50,17 +52,17 @@ class Player extends Unit {
         chalk.bgYellow(`${this.name}이 스테이지를 클리어하여 ${healAmount}만큼 HP를 회복했습니다.`),
       );
     } else if (skillRatio !== 0) {
-      console.log(chalk.bgYellow(`${this.name}이 스킬로 인해 ${healAmount}만큼 HP를 회복했습니다.`));
+      Battle.initBattleLogs(chalk.bgYellow(`[${logs.length}] ${this.name}이 스킬로 인해 ${healAmount}만큼 HP를 회복했습니다.`), logs);
     }
     this.hp += healAmount;
   }
 
   async action(player, monster, logs) {
-    console.log(chalk.green(`\n1. 공격한다 2. 아무것도 하지않는다.`));
+    console.log(chalk.green(`\n1. 공격한다 2. 아무것도 하지않는다. 3. 도망 간다`));
     const choice = readlineSync.question('당신의 선택은? '); 
 
     // 플레이어의 선택에 따라 다음 행동 처리
-    console.log(chalk.green(`[${++logs.length}] ${choice}를 선택하셨습니다.\n`));
+    Battle.initBattleLogs(chalk.green(`[${logs.length}] ${choice}를 선택하셨습니다.`), logs);
 
     return new Promise((resolve, reject) => {
       setTimeout(() => {
@@ -73,7 +75,7 @@ class Player extends Unit {
             break;
           case '3':
             if (player.escape(logs) === true) {
-              return;
+              player.isRun = true;
             }
             break;
         }
@@ -107,11 +109,12 @@ class Stage {
     this.player = player;
     this.totalStage = stageLength;
     this.currentStage = 1;
-    this.logs = [];
+    this.battleLogs = [];
     this.turn = [];
   }
 
-  displayStatus(player, monster, turn) {
+  // 스테이지의 상황을 
+  displayStageInfo(player, monster, turn) {
     let turnMessage = '[턴 순서] ';
     turn.forEach((turn, i) => {
       turnMessage += `| ${i + 1} 순위 : ${turn.name} `;
@@ -132,14 +135,16 @@ class Stage {
 
   async startStage(player, monster) {
     console.clear();
+    this.battleLogs=[];
+    console.log(chalk.magentaBright(`=== Stage ${this.currentStage} ===`));
+    console.log(chalk.inverse(`\n${monster.name}을 조우했다!`));
     while (player.hp > 0 || monster.hp > 0) {
       console.clear();
-      console.log(chalk.magentaBright(`=== Stage ${this.currentStage} ===`));
-      console.log(chalk.inverse(`\n${monster.name}을 조우했다!`));
       const turnOrder = Battle.checkTurn(player, monster);
-      this.displayStatus(player, monster, turnOrder);
-      this.logs.forEach((log) => console.log(log));
-      const battleEnd = await Battle.battleStart(player, monster, turnOrder, this.logs);
+      this.displayStageInfo(player, monster, turnOrder);
+      await Battle.displyBattleLogs(this.battleLogs);
+      
+      const battleEnd = await Battle.battleStart(player, monster, turnOrder, this.battleLogs);
       if(battleEnd) {
         break;
       }
@@ -150,10 +155,17 @@ class Stage {
     // 스테이지의 상황을 체크하는 시퀀스입니다.
     // 몬스터 처치 / 패배 / 클리어 => 3가지로 나뉩니다.
     if (monster.hp <= 0) {
-      console.log(chalk.bgGreen(`[${++this.logs.length}] ${monster.name}이 쓰러졌습니다.`));
-      player.heal(this.currentStage, 0, this.logs);
+      console.log(chalk.bgGreen(`[${++this.battleLogs.length}] ${monster.name}이 쓰러졌습니다.`));
+      player.heal(this.currentStage, 0, this.battleLogs);
       const next = readlineSync.question('\n다음 스테이지로 넘어가려면 아무키나 입력하십시오.');
       this.logs = [];
+      return true;
+    }
+
+    if (player.isRun === true) {
+      const next = readlineSync.question('\n다음 스테이지로 넘어가려면 아무키나 입력하십시오.');
+      this.logs = [];
+      player.isRun = false;
       return true;
     }
 
@@ -161,14 +173,14 @@ class Stage {
     if (player.hp <= 0) {
       console.log(chalk.bgRed(`\n${player.name} 이 쓰러졌습니다.`));
       console.log(chalk.redBright(`\n=== Game Over ===`));
-      const close = readlineSync.question('\n패배하였습니다. 다시 도전하세요!.');
+      const close = readlineSync.question('\n패배하였습니다. 다시 도전하세요!.\n');
       return false;
     }
 
     // 게임 종료 - 스테이지 클리어 시퀀스
     if (this.currentStage >= 10) {
       console.log(chalk.greenBright(`\n=== 게임 승리 ===`));
-      const close = readlineSync.question('\n축하합니다!, 당신은 승리했습니다!');
+      const close = readlineSync.question('\n축하합니다!, 당신은 승리했습니다!\n');
       return false;
     }
   }
@@ -176,6 +188,19 @@ class Stage {
 
 // 배틀에 관련된 메서드들을 정리한 클래스
 class Battle {
+
+  static async displyBattleLogs(logs) {
+    logs.forEach(log => {
+      console.log(log);
+    })
+  }
+
+  // message를 넣어주면 자동으로 배틀로그에 넣어주고 출력해주는 함수
+  static initBattleLogs(message, logs) {
+    logs.push(message);
+    console.log(message);
+  }
+
   // 배틀을 시작하는 정적메서드
   // 이 안에서 턴을 정리하고 배틀 인풋을 수행하게 된다.
   static async battleStart(player, monster, turnOrder, logs) {
@@ -195,6 +220,9 @@ class Battle {
   static async endCheck(player, monster) {
     // 몬스터 혹은 유저의 체력이 0인지 확인하는 구역입니다
     if(player.hp <= 0 || monster.hp <=0) {
+      return true;
+    }
+    else if (player.isRun === true) {
       return true;
     }
     return false;

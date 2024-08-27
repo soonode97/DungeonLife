65715db69dcacb79184091050e3b87a5f13bc531
soonode97 <soonode951@gmail.com>
2024-08-27 15:33:54 +0900

    기능 업데이트 및 수정
    스테이지 보너스, 배틀 이후 초기화, 회피, 선택지 반복

--- service/logs.js
+++ service/logs.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
 import chalk from 'chalk';
+import { delay } from './utils.js';
 
 // 배틀로그에 로그를 삽입하는 함수
 function initLogs(message, logs) {
@@ -25,16 +26,29 @@ export function displayStageInfo(player, monster, currentStage, turn) {
   console.log(
     chalk.inverse(`${monster.name} 을 조우했다\n`) +
       chalk.cyanBright(`\n| Stage: ${currentStage} |\n\n`) +
-      chalk.blueBright(`| Player HP: ${player.hp} | Evasion : ${player.evasion}% | Nabi : ${player.passive[0].currentStack}\n\n`) +
-      chalk.redBright(`| Monster HP: ${monster.hp} | Evasion : ${monster.evasion}% |\n`) +
+      chalk.blueBright(
+        `| Player HP: ${player.hp} | Evasion : ${player.evasion}% | ${player.actionTypes[0].name} : ${player.actionTypes[0].minDamage}~${player.actionTypes[0].maxDamage} | ${player.actionTypes[1].name} : ${player.actionTypes[1].minDamage}~${player.actionTypes[1].maxDamage} |\n\n`,
+      ) +
+      chalk.redBright(
+        `| Monster HP: ${monster.hp} | Evasion : ${monster.evasion}% | ${monster.actionTypes[0].name} : ${monster.actionTypes[0].minDamage}~${monster.actionTypes[0].maxDamage} | ${monster.actionTypes[1].name} : ${monster.actionTypes[1].minDamage}~${monster.actionTypes[1].maxDamage} |\n`,
+      ) +
       chalk.yellow(`\n${turnMessage}\n`),
   );
   console.log(chalk.magentaBright(`=====================\n`));
 }
 
 // 공격 관련 상황을 출력해주는 함수
-export function attackMessage(attacker, target, logs, damage, attackName) {
+export function attackMessage(attacker, target, logs, damage, attackName, isCritical, isEvasion) {
   initLogs(chalk.bgBlack(`[${logs.length}] ${attacker.name}이 ${attackName}를 시전합니다.`), logs);
+
+  if (isEvasion) {
+    initLogs(chalk.yellow(`[${logs.length}] ${target.name}이 공격을 회피하였습니다!!`), logs);
+  }
+
+  if (isCritical) {
+    initLogs(chalk.underline(`[${logs.length}] 공격이 크리티컬로 들어갑니다!!`), logs);
+  }
+
   const attackMessage = `[${logs.length}] ${attacker.name}이 ${target.name}에게 ${damage}만큼 피해를 입혔습니다.`;
   attacker.unitType === 'user'
     ? initLogs(chalk.bgGreen(attackMessage), logs)
@@ -62,6 +76,13 @@ export function clearHealMessage(name, healAmount) {
   );
 }
 
+// 스킬 사용 후 피 회복을 출력해주는 함수
+export function skillHealMessage(name, healAmount, logs) {
+  console.log(
+    initLogs(chalk.bgYellow(`${name}이 나비 가르기를 클리어하여 ${healAmount}만큼 HP를 회복했습니다.`), logs),
+  );
+}
+
 // 선택 가능한 액션을 출력해주는 함수
 export function possibleActionMessage(actions) {
   let actionMessage = '';
@@ -72,25 +93,43 @@ export function possibleActionMessage(actions) {
 }
 
 // 선택한 액션을 배틀로그에 삽입해주는 함수
-export function chooseAction(logs) {
-  const choice = readlineSync.question('당신의 선택은? ');
-  initLogs(chalk.green(`[${logs.length}] ${choice}를 선택하셨습니다.`), logs);
-  return choice;
+export async function chooseAction(actions, logs) {
+  let choice = null;
+  // const choice = readlineSync.question('당신의 선택은? ');
+  // initLogs(chalk.green(`[${logs.length}] ${choice}를 선택하셨습니다.`), logs);
+
+  while(choice === null) {
+    choice = readlineSync.questionInt('당신의 선택은? ');
+    initLogs(chalk.green(`[${logs.length}] ${choice}를 선택하셨습니다.`), logs);
+
+    if(choice > actions.length || choice <= 0) {
+      console.log('선택지에 없는 행동입니다. 다시 선택해주세요.');
+      choice = null;
+    }
+  }
+
+  return new Promise (resolve => {
+    resolve(choice);
+  });
 }
 
 // 턴이 다음으로 넘어갈 때 출력해주는 함수
-export function nextTurn() {}
+export function nextTurn(logs) {
+  initLogs(chalk.bgWhite(`[${logs.length}] 다음 턴으로 넘어갑니다.`), logs);
+}
 
 // 스테이지가 넘어갈 때 출력해주는 함수
-export function nextStageMessage_clear(name, logs) {
+export function monsterDiedMessage(name, logs) {
   console.log(chalk.bgGreen(`[${++logs.length}] ${name}이 쓰러졌습니다.`));
-  const next = readlineSync.question('\n다음 스테이지로 넘어가려면 아무키나 입력하십시오.');
 }
 
-export function nextStageMessage_run() {
+// 도망으로 스테이지 넘어갈때 출력
+export function nextStageMessage() {
   const next = readlineSync.question('\n다음 스테이지로 넘어가려면 아무키나 입력하십시오.');
+  delay(2000);
 }
 
+//
 export function gameOverMessage(player) {
   console.log(chalk.bgRed(`\n${player.name} 이 쓰러졌습니다.`));
   console.log(chalk.redBright(`\n=== Game Over ===`));
@@ -100,4 +139,8 @@ export function gameOverMessage(player) {
 export function gameClearMessage() {
   console.log(chalk.greenBright(`\n=== 게임 승리 ===`));
   const close = readlineSync.question('\n축하합니다!, 당신은 승리했습니다!\n');
+}
+
+export function errorPush(message, logs) {
+  initLogs(message, logs);
 }
--- service/unit.js
+++ service/unit.js
@@ -1,7 +1,21 @@
 import { Battle } from './Battle.js';
-import { attackMessage, runMessage, possibleActionMessage, chooseAction } from './logs.js';
-import { getDamageMinToMax, getIsCritical, getCriticalDamage } from './utils.js';
-import { delay } from './utils.js';
+import {
+  attackMessage,
+  runMessage,
+  possibleActionMessage,
+  chooseAction,
+  skillHealMessage,
+} from './logs.js';
+import {
+  getMinToMax,
+  getIsCritical,
+  getCriticalDamage,
+  getIsEvasion,
+  getStageClearHeal,
+  getStageBuffDamage,
+  getSkillHeal,
+  getMonsterHpBuff,
+} from './utils.js';
 
 export class Unit {
   constructor(name, hp, speed, evasion, criticalChance, criticalRatio, type) {
@@ -13,24 +27,39 @@ export class Unit {
     this.criticalRatio = criticalRatio;
     this.unitType = type;
     this.isRun = false;
-    this.buff = [];
-    this.debuff = [];
+    this.isCritical = false;
+    this.isEvasion = false;
   }
 
   async attack(attackType, target) {
-    // 크리티컬인지 확인
-    const isCritical = await getIsCritical(this.criticalChance);
+    // 공격 대상이 회피하였는지 확인
+    target.isEvasion = getIsEvasion(target.evasion);
+    if (target.isEvasion === true) {
+      return 0;
+    } else if (target.isEvasion !== true) {
+      let damage;
+      // 크리티컬인지 확인
+      this.isCritical = getIsCritical(this.criticalChance);
 
-    // 크리티컬상황
-    // 크리티컬은 항상 maxDamage에서 치명타배율이 들어가는 것으로 함
-    if (isCritical) {
-      const damage = await getCriticalDamage(this.criticalRatio, attackType.maxDamage);
+      // 크리티컬상황
+      // 크리티컬은 항상 maxDamage에서 치명타배율이 들어가는 것으로 함
+      if (this.isCritical) {
+        damage = getCriticalDamage(this.criticalRatio, attackType.maxDamage);
+      } else {
+        damage = getMinToMax(attackType.minDamage, attackType.maxDamage);
+      }
+
       target.hp -= damage;
       return damage;
-    } else {
-      const damage = await getDamageMinToMax(attackType.minDamage, attackType.maxDamage);
-      target.hp -= damage;
-      return damage;
+    }
+  }
+
+  static stageDamageBuff(target, stage) {
+    for (let idx in target.actionTypes) {
+      if (target.actionTypes[idx].type === 'attack') {
+        target.actionTypes[idx].minDamage += getStageBuffDamage(stage);
+        target.actionTypes[idx].maxDamage += getStageBuffDamage(stage);
+      }
     }
   }
 }
@@ -39,26 +68,12 @@ export class Player extends Unit {
   constructor() {
     super('Violeto', 100, 60, 20, 20, 0.8, 'user');
     this.escapeChanceRatio = 20;
-    this.passive = [
-      {
-        name: '나비',
-        type: 'passive',
-        maxStack: 5,
-        currentStack: 0,
-      },
-    ];
     this.actionTypes = [
       {
         name: '우아한 베기',
         type: 'attack',
         minDamage: 10,
         maxDamage: 20,
-        debuffType: 'decreaseAttack',
-        debuffValue: 50,
-        debuffDuration: 1,
-        buffType: 'increaseEvasion',
-        buffValue: 5,
-        buffDuration: 1,
         cooltime: 0,
         currentCooltime: 0,
       },
@@ -67,9 +82,6 @@ export class Player extends Unit {
         type: 'attack',
         minDamage: 20,
         maxDamage: 30,
-        buffType: 'heal',
-        buffValue: 10,
-        buffDuration: 0,
         cooltime: 7,
         currentCooltime: 0,
       },
@@ -91,7 +103,7 @@ export class Player extends Unit {
   }
 
   stageClearHeal(stage) {
-    const healAmount = Math.floor(Math.random() * 10 + 10) * (1 + stage * 0.5);
+    const healAmount = getStageClearHeal(stage);
     this.hp += healAmount;
     return healAmount;
   }
@@ -99,23 +111,36 @@ export class Player extends Unit {
   async action(player, monster, logs) {
     // 쿨타임인 행동을 제외하고 할 수 있는 행동을 정리한다.
     const actions = await Battle.checkPossibleAction(this);
-    // 행동들을 출력한다.
     possibleActionMessage(actions);
-    // 행동들을 선택한다.
-    const choice = chooseAction(logs);
+    const choice = await chooseAction(actions, logs);
 
+    const choiceAction = actions[choice - 1];
     // 플레이어의 선택에 따라 다음 행동 처리
     // 입력에 대한 행동을 키값으로 관리하는게 좋겠다고 판단
-    if (actions[choice - 1].type === 'attack') {
-      // 스킬을 사용한 경우
-      if (actions[choice - 1] === this.actionTypes[1]) {
-        // 스킬 쿨타임 적용
-        this.actionTypes[1].currentCooltime = this.actionTypes[1].cooltime;
-      }
+    if (choiceAction.type === 'attack') {
+      const damage = await this.attack(choiceAction, monster);
+      attackMessage(
+        this,
+        monster,
+        logs,
+        damage,
+        choiceAction.name,
+        this.isCritical,
+        monster.isEvasion,
+      );
 
-      const damage = await this.attack(actions[choice - 1], monster);
-      attackMessage(this, monster, logs, damage, actions[choice - 1].name);
-    } else if (actions[choice - 1].type === 'run') {
+      // 스킬을 사용한 경우
+      if (choiceAction === this.actionTypes[1]) {
+        // 스킬 쿨타임 적용 및 피해량에 따른 피 회복
+        this.actionTypes[1].currentCooltime = this.actionTypes[1].cooltime;
+        const skillHealAmount = getSkillHeal(damage);
+        this.hp += skillHealAmount;
+        skillHealMessage(this.actionTypes[1].name, skillHealAmount, logs);
+      }
+    }
+
+    // 도망 시 로직 처리
+    else if (choiceAction.type === 'run') {
       this.isRun = this.tryRun();
       runMessage(this.isRun, this.name, logs);
     }
@@ -137,7 +162,7 @@ export class Monster extends Unit {
         hp: 50,
         speed: 60,
         evasion: 20,
-        criticalChance: 10,
+        criticalChance: 5,
         criticalRatio: 0.3,
         type: 'enemy',
         emersionChance: 90,
@@ -157,9 +182,6 @@ export class Monster extends Unit {
             maxDamage: 10,
             cooltime: 3,
             currentCooltime: 0,
-            debuffType: 'decreaseHit',
-            debuffValue: 5,
-            debuffDuration: 1,
           },
         ],
       },
@@ -168,7 +190,7 @@ export class Monster extends Unit {
         hp: 80,
         speed: 30,
         evasion: 10,
-        criticalChance: 20,
+        criticalChance: 10,
         criticalRatio: 0.5,
         type: 'enemy',
         emersionChance: 30,
@@ -196,7 +218,7 @@ export class Monster extends Unit {
         hp: 40,
         speed: 90,
         evasion: 30,
-        criticalChance: 35,
+        criticalChance: 30,
         criticalRatio: 0.3,
         type: 'enemy',
         emersionChance: 40,
@@ -215,9 +237,6 @@ export class Monster extends Unit {
             minDamage: 15,
             maxDamage: 20,
             cooltime: 3,
-            debuffType: 'decreaseAttack',
-            debuffValue: 50,
-            debuffDuration: 1,
             currentCooltime: 0,
           },
         ],
@@ -227,7 +246,7 @@ export class Monster extends Unit {
         hp: 200,
         speed: 30,
         evasion: 20,
-        criticalChance: 30,
+        criticalChance: 10,
         criticalRatio: 0.7,
         type: 'enemy',
         emersionChance: 2,
@@ -246,9 +265,6 @@ export class Monster extends Unit {
             minDamage: 30,
             maxDamage: 50,
             cooltime: 3,
-            currentCooltime: 0,
-            debuffType: 'decreaseAttack',
-            debuffValue: 50,
             debuffDuration: 1,
           },
         ],
@@ -287,28 +303,39 @@ export class Monster extends Unit {
       // 쿨타임인 행동을 제외하고 할 수 있는 행동을 정리한다.
       const actions = await Battle.checkPossibleAction(monster);
 
-      // 몬스터의 특수 공격이 쿨타임인지 확인하고,
-      // 쿨타임이 아닌 경우 반반확률로 특수 공격을 사용하도록 설정
-      // 특수 공격이 아닌 경우는 일반 공격을 하도록 설정
       let attackIs = {};
+
+      // 특수 공격 가능 여부를 확인
       if (actions[1] !== undefined) {
         const isSpecialAttack = Math.floor(Math.random() * 100);
         if (isSpecialAttack > 40) {
-          attackIs = await Object.assign(attackIs, actions[1]);
+          attackIs = actions[1];
           this.actionTypes[1].currentCooltime = this.actionTypes[1].cooltime;
+        } else {
+          // 일반 공격
+          attackIs = actions[0];
         }
-      }
-
-      // 일반 공격
-      else if (actions[1] === undefined) {
-        attackIs = await Object.assign(attackIs, actions[1]);
-        await Object.assign(attackIs, actions[0]);
-        this.actionTypes[1].currentCooltime--;
+      } else if (actions[0] !== undefined) {
+        // 특수 공격이 불가능하고 일반 공격만 가능한 경우
+        attackIs = actions[0];
       }
 
       // 공격 시작
       const damage = await this.attack(attackIs, player);
-      attackMessage(monster, player, logs, damage, attackIs.name);
+      attackMessage(
+        monster,
+        player,
+        logs,
+        damage,
+        attackIs.name,
+        this.isCritical,
+        player.isEvasion,
+      );
     }
+  }
+
+  stageHpBuff(stage) {
+    const hpAmount = getMonsterHpBuff(this, stage);
+    this.hp += hpAmount;
   }
 }
